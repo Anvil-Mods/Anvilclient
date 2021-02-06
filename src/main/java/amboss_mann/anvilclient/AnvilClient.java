a421060971cf118f5ac30cbba52673770e8711ef
Ambossmann <68373395+Ambossmann@users.noreply.github.com>
2021-02-06 13:25:31 +0100

    Initial commit

---
+++ src/main/java/amboss_mann/anvilclient/AnvilClient.java
@@ -0,0 +1,54 @@
+package amboss_mann.anvilclient;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.mojang.text2speech.Narrator;
+
+import net.minecraft.client.AbstractOption;
+import net.minecraft.client.settings.NarratorStatus;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("anvilclient")
+public class AnvilClient
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "anvilclient";
+
+    public AnvilClient() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+    	//AbstractOption.NARRATOR.setOptionValues((0));
+        AbstractOption.GAMMA.setMaxValue(12.0F);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
+    }
+    
+}
