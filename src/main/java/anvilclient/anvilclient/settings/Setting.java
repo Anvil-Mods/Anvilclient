2a7f82d9fbee3cac4d7f381198c8235e4e2fcb0b
Ambossmann <68373395+Ambossmann@users.noreply.github.com>
2021-05-04 17:50:46 +0200

    Update Setting.java

--- src/main/java/anvilclient/anvilclient/settings/Setting.java
+++ src/main/java/anvilclient/anvilclient/settings/Setting.java
@@ -17,12 +17,12 @@
 package anvilclient.anvilclient.settings;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RUNTIME)
 @Target(FIELD)
 public @interface Setting {
 
